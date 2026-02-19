e9a326af6fd7b5d8d10b278e99a0987f5bdb6e8b
Abhishek Verma <verma.ab23694@gmail.com>
2026-02-19 10:14:11 +0530

    Initial Commit

---
+++ multi-repo-ios/multi_repo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_repo_iosApp.swift
+//  multi-repo-ios
+//
+//  Created by abhishekverma on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct multi_repo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
